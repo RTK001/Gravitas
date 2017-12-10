dec9e838f59ed6313aaffd146bac5b85a801ad52
Rishi <rishikalra001@gmail.com>
2017-12-10 16:50:05 +0000

    Added an arrow pointing towards a goal to the UI

---
+++ GravitasProject/Assets/Goal Arrow/GoalUI.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalUI : MonoBehaviour {
+
+    public float ArrowDistanceFromShip = 10;
+
+
+    // Create Singleton
+    public static GoalUI instance = null;
+
+    // Arrow Prefab
+    public GameObject arrowPrefab;
+
+    // Arrow object
+    private GameObject arrow;
+
+    // set arrow offset
+    public Vector3 offset;
+
+    // The Goal
+    public GameObject goal;
+
+    // The playership, for ease of calculation
+    PlayerShip player;
+
+    
+
+    private void Awake()
+    {
+        if (instance == null)               // If the static value is null, set it to be this awakened instance.
+        {
+            instance = this;
+        }
+        else if (instance != null)          // if the value has been set to another instance of Playership, destroy this instance and preserve the other one.
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // get the goal for the scene if not set
+        if (goal == null)
+        {
+            goal = GameObject.Find("GoalPrefab");
+        }
+
+        offset = new Vector3(-20 ,20, 20);
+
+        // Create the arrow, and set parameters
+        arrow = Instantiate(arrowPrefab);
+        arrow.transform.SetParent(this.transform);
+        arrow.transform.position += offset;
+        arrow.transform.rotation = this.transform.rotation ;
+        arrow.transform.localScale = new Vector3(5, 5, 5);
+
+        // Get the player to reference later
+        player = Object.FindObjectOfType<PlayerShip>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Set the arrow location to be in the direction of the goal
+
+        // Get the direction vector from the ship to the goal
+        Vector3 vecToGoal = goal.transform.position - player.transform.position;
+        // Set the arrow position to be ArrowDistanceFromShip in the direction of the goal from the ship
+        arrow.transform.position = player.transform.position + vecToGoal.normalized * ArrowDistanceFromShip;
+
+
+        // Set the arrow rotation to be away from the player
+
+        // create a relative vector from the player to the arrow, but set the z component equal to zero.
+        Vector3 temp = new Vector3(arrow.transform.position.x - player.transform.position.x, arrow.transform.position.y - player.transform.position.y, 0);
+        // create a rotation quaternion, set to be from the arrow to the player
+        Quaternion rot = Quaternion.LookRotation(temp);
+        // Set the arrow to be "looking at" the player. Add some angles to ensure the arrows are rotated correctly.
+        arrow.transform.rotation = rot * Quaternion.Euler(-90, -90, 0);
+
+    }
+    
+}
---
+++ GravitasProject/Assets/Player and UI/Ship.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+
+    // Properties: Things a ship has
+
+    protected Rigidbody rigid;
+    Vector3 shipForward;
+    Vector3 shipRight;
+    Vector3 shipUp;
+
+    // Engine Limits
+    public float maxSpeed = 1f;
+    float sqrMaxSpeed;
+
+    
+    public void Move(float forward, float strafeRight, float rotRight)
+    {
+        // Function called to move the ship using forces
+        // References the ship axes
+
+        rigid.AddForce((shipForward * forward + shipRight * strafeRight));
+        rigid.AddTorque(shipUp * (-rotRight));
+
+    }
+
+    protected void LimitSpeed()
+        // A function to limit the speed of the ship.
+        // Typically used for enemies, but may be a useful feature for the player later.
+    {
+        if (rigid.velocity.sqrMagnitude > sqrMaxSpeed)  // If the ship's speed is greater than the limit (computed in squares for speed)
+        {
+
+            rigid.velocity = rigid.velocity.normalized * sqrMaxSpeed;       // set the ship velocity to the speed limit in the current direction.
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        // Check if the Ship has all the requisite components
+        // Output a Debug.Log if not
+
+        // Get the rigid body
+        rigid = this.GetComponent<Rigidbody>();
+        if (rigid == null)
+        {
+            Debug.Log("Ship is  missing a Rigid Body Component", this);         // If not, alert the debugger that a rigid body must be assigned.
+        }
+
+        // Set the axes of the ship
+        // Currently, all ships are created at 90 Degrees around X to the origin.
+        shipForward = this.transform.up;
+        shipRight = this.transform.right;
+        shipUp = this.transform.forward;
+
+        // Set the speed Limit square
+        sqrMaxSpeed = maxSpeed * maxSpeed;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
